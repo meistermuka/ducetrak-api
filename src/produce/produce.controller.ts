e9f68662a6a65b42a3d16bfea326b91705e24d75
Mukandila Mukandila <ndila@muka.io>
2020-01-24 20:21:16 -0500

    Some code formatting and stuff. Nothing official

--- src/produce/produce.controller.ts
+++ src/produce/produce.controller.ts
@@ -11,53 +11,53 @@ import { ProduceService } from './produce.service';
 @Controller('produce')
 export class ProduceController {
 
-    constructor(private produceService: ProduceService) {}
+  constructor(private produceService: ProduceService) {}
 
-    @Get()
-    async getAllProduce(): Promise<Produce[]> {
-        const allProduce = await this.produceService.getAllProduce();
-        if(allProduce.length === 0) {
-            throw new NotFoundException();
-        }
-        return allProduce;
+  @Get()
+  async getAllProduce(): Promise<Produce[]> {
+    const allProduce = await this.produceService.getAllProduce();
+    if(allProduce.length === 0) {
+      throw new NotFoundException();
     }
+    return allProduce;
+  }
 
-    @Get(':id')
-    async getProduce(@Param('id') id: number): Promise<Produce> {
-        const produce = await this.produceService.getProduce(id);
-        if(isEmpty(produce)) {
-            throw new NotFoundException();
-        }
-        return produce;
+  @Get(':id')
+  async getProduce(@Param('id') id: number): Promise<Produce> {
+    const produce = await this.produceService.getProduce(id);
+    if(isEmpty(produce)) {
+      throw new NotFoundException();
     }
+    return produce;
+  }
 
-    @Post()
-    async postProduce(@Body() produceDto: ProduceDto): Promise<void> {
-        try {
-            await this.produceService.postProduce(produceDto);
-        } catch (e) {
-            console.log(e);
-            throw new BadRequestException();
-        }
+  @Post()
+  async postProduce(@Body() produceDto: ProduceDto): Promise<void> {
+    try {
+      await this.produceService.postProduce(produceDto);
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
     }
+  }
 
-    @Put(':id')
-    async updateProduce(@Param('id') id: number, @Body() produceDto: ProduceDto): Promise<void> {
-        try {
-            await this.produceService.updateProduce(id, produceDto);
-        } catch (e) {
-            console.log(e);
-            throw new BadRequestException();
-        }
+  @Put(':id')
+  async updateProduce(@Param('id') id: number, @Body() produceDto: ProduceDto): Promise<void> {
+    try {
+      await this.produceService.updateProduce(id, produceDto);
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
     }
+  }
 
-    @Delete(':id')
-    async deleteProduce(@Param('id') id: number): Promise<void> {
-        try {
-            await this.produceService.deleteProduce(id);
-        } catch (e) {
-            console.log(e);
-            throw new BadRequestException();
-        }
+  @Delete(':id')
+  async deleteProduce(@Param('id') id: number): Promise<void> {
+    try {
+      await this.produceService.deleteProduce(id);
+    } catch (e) {
+      console.log(e);
+      throw new BadRequestException();
     }
+  }
 }
--- src/produce/produce.service.ts
+++ src/produce/produce.service.ts
@@ -15,88 +15,86 @@ export class ProduceService {
     constructor(private priceService: PriceService) {}
 
     async getProduce(id: number): Promise<Produce> {
-        return await Produce.findOne({ id }, { relations: ['location', 'user', 'type', 'price'] });
+      return await Produce.findOne({ id }, { relations: ['location', 'user', 'type', 'price'] });
     }
 
     async getAllProduce(): Promise<Produce[]> {
-        return await Produce.find({ relations: ['location', 'user', 'type'] });
+      return await Produce.find({ relations: ['location', 'user', 'type'] });
     }
 
     private async createOrUpdateProduce(produce: Produce, produceDto: ProduceDto): Promise<void> {
-        produce.name = produceDto.name;
+      produce.name = produceDto.name;
 
-        if(!isEmpty(produceDto.location)) {
-            const location = await Location.findOne({ id: produceDto.location });
-            if(isEmpty(location)) {
-                throw new Error('No Location Found');
-            }
-            produce.location = location;
+      if(!isEmpty(produceDto.location)) {
+        const location = await Location.findOne({ id: produceDto.location });
+        if(isEmpty(location)) {
+            throw new Error('No Location Found');
         }
+        produce.location = location;
+      }
 
-        if(!isEmpty(produceDto.user)) {
-            const user = await User.findOne({ id: produceDto.user });
-            if(isEmpty(user)) {
-                throw new Error('No User Found');
-            }
-            produce.user = user;
+      if(!isEmpty(produceDto.user)) {
+        const user = await User.findOne({ id: produceDto.user });
+        if(isEmpty(user)) {
+            throw new Error('No User Found');
         }
+        produce.user = user;
+      }
 
-        if(!isEmpty(produceDto.type)) {
-            const type = await Type.findOne({ id: produceDto.type });
-            if(isEmpty(type)) {
-                throw new Error('No Type Found');
-            }
-            produce.type = type;
+      if(!isEmpty(produceDto.type)) {
+        const type = await Type.findOne({ id: produceDto.type });
+        if(isEmpty(type)) {
+            throw new Error('No Type Found');
         }
-
+        produce.type = type;
+      }
     }
 
     async postProduce(produceDto: ProduceDto): Promise<void> {
-        const produce = new Produce();
+      const produce = new Produce();
 
-        await this.createOrUpdateProduce(produce, produceDto);
+      await this.createOrUpdateProduce(produce, produceDto);
 
-        produce.createdDate = new Date().toISOString();
-        produce.modifiedDate = new Date().toISOString();
-        
-        const insertedProduce = await produce.save();
+      produce.createdDate = new Date().toISOString();
+      produce.modifiedDate = new Date().toISOString();
 
-        const produceId = insertedProduce.id;
-        const user = insertedProduce.user;
+      const insertedProduce = await produce.save();
 
-        if (isEmpty(produceDto.price)) {
-            throw new Error('No Price Found');
-        }
+      const produceId = insertedProduce.id;
+      const user = insertedProduce.user;
 
-        try {
-            await this.priceService.postPrice(produceId, user, produceDto.price);
-        } catch (e) {
-            console.log(e);
-        }
-        
+      if (isEmpty(produceDto.price)) {
+          throw new Error('No Price Found');
+      }
+
+      try {
+        await this.priceService.postPrice(produceId, user, produceDto.price);
+      } catch (e) {
+        console.log(e);
+      }
+
     }
 
     async updateProduce(id: number, produceDto: ProduceDto): Promise<void> {
-        const produce = await Produce.findOne({ id }, { relations: ['location', 'user', 'type'] });
-        await this.createOrUpdateProduce(produce, produceDto);
-        produce.modifiedDate = new Date().toISOString();
+      const produce = await Produce.findOne({ id }, { relations: ['location', 'user', 'type'] });
+      await this.createOrUpdateProduce(produce, produceDto);
+      produce.modifiedDate = new Date().toISOString();
 
-        if(!isEmpty(produceDto.price)) {
-            try {
-                await this.priceService.updatePrice(id, produce.user, produceDto.price);
-            } catch (e) {
-                console.log(e);
-            }
+      if(!isEmpty(produceDto.price)) {
+        try {
+          await this.priceService.updatePrice(id, produce.user, produceDto.price);
+        } catch (e) {
+          console.log(e);
         }
-
-        produce.save();
+      }
+      produce.save();
     }
 
     async deleteProduce(id: number): Promise<void> {
-        const produce = await Produce.findOne({id})
-        if(isEmpty(produce)) {
-            throw new Error('No Produce Found');
-        }
-        await Produce.delete({ id });
+      const produce = await Produce.findOne({id})
+      if(isEmpty(produce)) {
+        throw new Error('No Produce Found');
+      }
+      await Produce.delete({ id });
     }
 }
