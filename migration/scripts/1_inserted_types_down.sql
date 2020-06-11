e920662cbfd09a41c2a8cca14da541b0dba9ea57
Mukandila Mukandila <ndila@muka.io>
2020-06-11 19:17:58 -0400

    SQL migration scripts

---
+++ migration/scripts/1_inserted_types_down.sql
@@ -0,0 +1,1 @@
+DELETE FROM type WHERE name IN ('fruit', 'vegetable', 'grain', 'bean', 'nut', 'seed');
---
+++ migration/scripts/1_inserted_types_up.sql
@@ -0,0 +1,6 @@
+INSERT INTO type (name, "createdDate", "updatedDate") VALUES ('fruit', NOW()::timestamp, NOW()::timestamp);
+INSERT INTO type (name, "createdDate", "updatedDate") VALUES ('vegetable', NOW()::timestamp, NOW()::timestamp);
+INSERT INTO type (name, "createdDate", "updatedDate") VALUES ('grain', NOW()::timestamp, NOW()::timestamp);
+INSERT INTO type (name, "createdDate", "updatedDate") VALUES ('bean', NOW()::timestamp, NOW()::timestamp);
+INSERT INTO type (name, "createdDate", "updatedDate") VALUES ('nut', NOW()::timestamp, NOW()::timestamp);
+INSERT INTO type (name, "createdDate", "updatedDate") VALUES ('seed', NOW()::timestamp, NOW()::timestamp);
